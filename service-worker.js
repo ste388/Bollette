9fa09b7c375886c247bcd06e5780c3e3b57832f6
ste388 <74853234+ste388@users.noreply.github.com>
2025-12-16 11:27:47 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,51 +1,28 @@
-const CACHE_NAME = 'bollette-v2';
+const CACHE_NAME = 'bollette-v3'; // Ho cambiato versione per forzare l'aggiornamento
 const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json',
+  '.',
+  'index.html',
+  'manifest.json',
   './icons/icon-192x192.png',
   './icons/icon-512x512.png',
-  // IMPORTANTE: Cache delle librerie esterne per far funzionare il PDF reader offline
-  'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.16.105/pdf.min.js',
+ 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.16.105/pdf.min.js',
   'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.16.105/pdf.worker.min.js'
 ];
 
-// Installazione: scarica i file
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(function(cache) {
-        console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
   );
 });
 
-// Richiesta: serve i file dalla cache se disponibili
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request)
       .then(function(response) {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
+        return response || fetch(event.request);
       })
-  );
-});
-
-// Attivazione: pulisce vecchie cache se cambi versione
-self.addEventListener('activate', function(event) {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
   );
 });
